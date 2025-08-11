eadc1ac450f5edd5baedbd7e14ad30be3be4a2f5
AnnaRoska <murkesha2014@gmail.com>
2025-08-11 20:49:30 +0300

    finish

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -7,12 +7,6 @@ const form = document.querySelector(".feedback-form");
 const STORAGE_KEY = 'feedback-form-state';
 
 
-form.addEventListener('input', (e) => {
-formData[e.target.name] = e.target.value;
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData)); 
-}
-)
-
 const saved = localStorage.getItem(STORAGE_KEY);
 if (saved) {
   try {
@@ -23,6 +17,13 @@ if (saved) {
     console.error('Error parsing saved form data:', error);
   }
 }
+
+form.addEventListener('input', (e) => {
+formData[e.target.name] = e.target.value;
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData)); 
+}
+)
+
 
 form.addEventListener('submit', event => {
   event.preventDefault();
